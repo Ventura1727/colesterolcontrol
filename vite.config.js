506e2caed2bd34eb1ca1bcfc0937485a8cdefff7
Ventura1727 <v_ventura@outlook.com>
2026-01-09 10:26:53 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import base44 from "@base44/vite-plugin"
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   logLevel: 'error',
   plugins: [
