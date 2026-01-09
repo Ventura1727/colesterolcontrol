96f55619411b92094ff623569fd50d19c89fd664
Ventura1727 <v_ventura@outlook.com>
2026-01-09 10:04:49 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,9 +11,10 @@ export default defineConfig({
     react(),
   ],
   define: {
+    // Escrevendo o valor real direto no código para matar o 'null'
     'process.env.BASE44_API_URL': JSON.stringify('https://api.base44.com'),
     'process.env.BASE44_APP_ID': JSON.stringify('a0dbae3c2dfb4a2988b0a3b4dad8ca1f'),
-    'import.meta.env.BASE44_API_URL': JSON.stringify('https://api.base44.com'),
-    'import.meta.env.BASE44_APP_ID': JSON.stringify('a0dbae3c2dfb4a2988b0a3b4dad8ca1f')
+    'import.meta.env.VITE_BASE44_API_URL': JSON.stringify('https://api.base44.com'),
+    'import.meta.env.VITE_BASE44_APP_ID': JSON.stringify('a0dbae3c2dfb4a2988b0a3b4dad8ca1f'),
   }
 })
